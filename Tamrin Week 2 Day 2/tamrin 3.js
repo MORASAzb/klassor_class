1da4f2108cfb82dc1e231d925f8e8db305a1c623
4tendev <4tenprog@gmail.com>
2023-08-18 23:28:23 +0330

    Update tamrin 3.js

--- Tamrin Week 2 Day 2/tamrin 3.js
+++ Tamrin Week 2 Day 2/tamrin 3.js
@@ -1,5 +1,7 @@
 const firstName=prompt("Your first name")
 const lastName=prompt("Your last name")
 console.log(`${firstName} ${lastName}`)
-const lenghtOfFullName=firstName.length + lastName.length
+let noSpaceFirstName=firstName.trim().split(" ").join("")
+let noSpaceLastName=lastName.trim().split(" ").join("")
+const lenghtOfFullName=noSpaceFirstName.length + noSpaceLastName.length
 console.log(lenghtOfFullName)
