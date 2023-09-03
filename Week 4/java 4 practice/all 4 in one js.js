2b6805e655bbd604b4140432ff229a0ae8b9dfa6
4tendev <4tenprog@gmail.com>
2023-09-03 14:10:59 +0330

    4 practice java

---
+++ Week 4/java 4 practice/all 4 in one js.js
@@ -0,0 +1,150 @@
+// You can test all te practic interactivly with opening html file
+const vowelLetters = ["a","A","e","E","o","O","i","I","u","U"]
+function isPositiveNumber(input) 
+    {
+        const number = parseFloat(input);
+        return !isNaN(number) && number > 0;
+    }
+function isNumber(input) 
+    {
+        const number = parseFloat(input);
+        return !isNaN(number) ;
+    }
+
+function practiceOne(str)
+    {      
+        let answer = str
+        answer=answer.split("").filter((value) => !vowelLetters.includes(value) ).join("")
+        return answer
+    }
+    function powerTwo(digit) {
+        return digit ** 2
+        
+    }
+function practiceTwo(array) {
+        let names = "Nobody"
+        console.log(array)
+        console.log(array.length)
+        if (array.length > 1 ){
+            array[array.length - 1] = " and " + array[array.length - 1];
+            names = array.join(", ").replace((", "+array.at(-1)), array.at(-1))
+        }
+        else if(array.length === 1){
+            names = array[0]
+        }
+        let like = array.length < 2 ? "likes" : "like"
+        
+        const answer = `${names} ${like} this.` 
+        return answer
+    
+}
+function practiceThree(array,array2) {
+    answer =  array
+    answer=answer.filter((value => !array2.includes(value)))
+    return answer
+
+}
+function practiceFour(str) {
+    let answer = 0
+    array=str.toUpperCase().split("")
+    while (array.length != 0) {
+        const toCheck = array[0]
+        array.shift()
+        if (array.includes(toCheck) ) {
+            answer++   
+            array=array.filter((value)=> !(value==toCheck)) 
+        }
+    }
+    return answer
+}
+let needToTestMore = true    
+while (needToTestMore) {
+    const practiceNumber=prompt("tell me witch practice you wanna test amir (Only digit)?")
+    let answer
+    if (isPositiveNumber(practiceNumber) && practiceNumber<5)
+        {   
+            let userInputCondition =false
+            let userInput
+            switch ( practiceNumber) {
+                case "1":
+                    userInput=prompt("Give me a anything !?")
+                    answer=practiceOne(userInput)
+                    break;
+            
+                case "2":
+                    let userInputs = []
+                    while (true)
+                    {
+                        userInput=prompt(`Give me names one by one. when you done enter -1 !?`)
+                        if (userInput != "-1"){
+                        userInputs.push(userInput)} 
+                        else{
+                                break
+                            }
+                    }
+                    answer=practiceTwo(userInputs) 
+                    break;
+                case "3":
+                    let userInputs1 = []
+                    let userInputs2 = []
+                    while (true)
+                    {
+                        userInput=prompt("Give me a number for first array! (-1 to go for next array)?")
+
+                        if (isNumber(userInput)) {
+                            if (userInput != "-1") {
+                                userInputs1.push(userInput)
+                            }
+                            else{
+                                break
+                            }
+                        }
+                        else{
+                            answer = "You need to enter a digit and positive !"
+                            userInputCondition = false
+                            alert (answer)
+                        }
+                    }
+                    while (true)
+                    {
+                        userInput=prompt("Give me a number for second array! (-1 when you done)?")
+
+                        if (isNumber(userInput)) {
+                            if (userInput != "-1") {
+                                userInputs2.push(userInput)
+                            }
+                            else{
+                                break
+                            }
+                        }
+                        else{
+                            answer = "You need to enter a digit and positive !"
+                            userInputCondition = false
+                            alert (answer)
+                        }
+                    }
+                    answer=practiceThree(userInputs1,userInputs2)
+                    break; 
+                case "4":
+                    userInput=prompt("Give me anything?")
+                    answer=practiceFour(userInput)                   
+                    break;
+            }
+            alert (answer)
+        }
+    else
+        {   
+            if (!isPositiveNumber(practiceNumber))
+                {
+                    answer= "Told you only digit !!!!!!!!!"
+                }
+            else
+                {
+                    answer= "You gave us only 4 practice to solve !!!!"
+                }
+            alert (answer)
+            continue
+        } 
+    needToTestMore =confirm("Do you wanna test more practice ?")
+    continue
+}
